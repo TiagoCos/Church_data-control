37ecad83f19c2b05db9bd3d0f5f52df9f63fad6a
Your NameTiagoCos <tiagocostasilva27@gmail.com>
2022-12-10 18:25:13 -0300

    Church

--- src/pages/Dashboard/Home.js
+++ src/pages/Dashboard/Home.js
@@ -3,38 +3,44 @@ import { Alert } from 'antd';
 import Marquee from 'react-fast-marquee' ;
 import React from 'react';
 import 'antd/dist/antd.css';
+import { useState } from 'react';
 
 function Home(){
+  const [cargo , setCargo] = useState('')
 
   const msg = 'teste de mensagem'
+
      return(
-     <main className='principal'>
+  <main className='principal'>
     <section id='1' className='apresentacao'>
-    <div className='Sub-div'> 
-          <div className='Loop-Banner'>
+      <div className='Sub-div'> 
+         <div className='Loop-Banner'>
            <Alert
               banner
                 message={
                <Marquee pauseOnHover gradient={false}>
                <strong>{msg}</strong> 
                </Marquee>
-               }/>
-          </div>
-
-         <h3>Conheça nosso site </h3>
-         <p>baixe as informações mais relevantes pra você!</p>
+               }/></div>
+        <h3 id='info'>Conheça nosso site </h3>
+        <p id='info'>Confira as informações mais relevantes pra você!</p>
         </div>
    </section>  
-             <section id='b' className='diretoria'>
-             </section>
+          <section id='b' className='diretoria'>
+            <h3>Diretoria e direção da igreja.</h3>
+              <div className='imagem-presidente'></div>
+               <h2>{cargo}</h2>
+                 
 
-              <section id='c' className='filiacao'>
+                 </section>
+
+           <section id='c' className='filiacao'>
               </section>
 
-               <section id='d' className='estatuto'>
+            <section id='d' className='estatuto'>
                </section>
 
-                <section id='2' className='presidente'>
+             <section id='2' className='presidente'>
                 </section>
           
         </main>

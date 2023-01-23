531332e4fa9942a763240208b820b16c2b10436b
Your NameTiagoCos <tiagocostasilva27@gmail.com>
2023-01-22 22:21:53 -0300

    Church

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from './pages/Login/Login';
 import Footer from './Components/layout/Footer/footer';
 import Header from './Components/header/Header';
 import Cadastro from './pages/Cadastro/Cadastro';
+import  Secretario from './pages/Secretário/Secretario';
 
 function App() {
   //Rotas 
@@ -14,9 +15,10 @@ function App() {
        <Header/>
       
         <Routes> {/*esse seria o Switch */}
-          <Route  path='/' element={<Home />}/>
+          <Route path='/' element={<Home />}/>
           <Route path='/Login' element={<Login/>}/>
           <Route path='/Cadastro' element={<Cadastro/>}/>
+          <Route path='/Secretario' element={<Secretario/>}/>
         </Routes>
 
        <Footer/>
---
+++ src/pages/Secretário/Secretario.js
@@ -0,0 +1,14 @@
+import "../Secretário/index.Secretario.css"
+
+function Secretario(){
+    return(
+        <main className="Secretario-main">
+            <div className="card-cadastro">cadastro de membros</div>
+            <div className="card-cadastro">membros inativos</div>
+            <div className="card-cadastro">membros ativos</div>
+        </main>
+    )
+}
+
+
+export default Secretario

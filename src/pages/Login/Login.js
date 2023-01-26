334c5e3a64472c9a098ac5fce87ff9182d6fdb42
Tiago Costa Silva <tiagocostasilva27@gmail.com>
2023-01-26 10:41:55 -0300

    Church

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,37 +1,104 @@
+import React from 'react';
 import '../Login/indexLogin.css'
 import { Link } from 'react-router-dom';
-function Login() {
+import { useState } from 'react';
 
-/*const teste ={
-  height: '160px',
-  color: '#fff',
-  textAlign: 'center',
-  background: '#a52a2a',
-  border: '1px solid blak',
-}*/
 
+const Login = () => {
+
+const RegisterForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [usernameError, setUsernameError] = useState('');
+  const [isUsernameAvailable, setIsUsernameAvailable] = useState(false);
+
+  const handleUsernameChange = async (event) => {
+    const value = event.target.value;
+    setUsername(value);
+    const isAvailable = await checkUsernameAvailability(value);
+    setIsUsernameAvailable(isAvailable);
+    setUsernameError(isAvailable ? '' : 'username already taken');
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!validate()) {
+      return;
+    }
+    // Submit the form
+  };
+
+  const checkUsernameAvailability = async (username) => {
+    // make a request to the server to check if the
+  }
+}
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!validate()) {
+      return;
+    }
+    // Submit the form
+  };
+
+  const validate = () => {
+    if (!username) {
+      setError('Username is required');
+      return false;
+    }
+    if (!password) {
+      setError('Password is required');
+      return false;
+    }
+    if (password.length < 8) {
+      setError('Password must be at least 8 characters');
+      return false;
+    }
+    setError('');
+    return true;
+  };
 
   return (
-    <main className='mainL'>
-        <div className='container-login'>   
-          <h1>Login</h1>
-          <form>
-            <label >CPF: </label>
-              <input type='CPF' name='CPF' id='CPF' placeholder='Digite seu CPF' autoComplete='off'></input>
-                  <div className="campo">
-                    <label>Categoria: </label>
-                    <select id="Categoria" required>
-                      <option selected disabled >Selecione</option>
-                      <option>Secretário</option>
-                      <option>Tesoureiro</option>
-                      <option>Administrador</option>
-                    </select>
-                  </div>
-              <button></button>
-              <label >Senha: </label>
-              <input type='password' placeholder='Digite sua senha'></input>
-          </form>
-          <button className='button-form' type='submit'>
+    <div className='mainL'>
+    <form className='container-login' onSubmit={handleSubmit}>
+      <label>
+        CPF:
+        <input type="text" value={username} placeholder='Digite seu CPF' autoComplete='' onChange={handleUsernameChange} />
+      </label>
+      <br />
+      <div className="campo">
+        <label>Categoria: </label>
+        <select id="Categoria" required>
+           <option selected disabled >Selecione</option>
+           <option>Secretário</option>
+           <option>Tesoureiro</option>
+          <option>Administrador</option>
+       </select>
+      </div>
+      <br />
+      <label>
+        Senha:
+        <input type="password" value={password} placeholder='Digite seu Senha' onChange={handlePasswordChange} />
+      </label>
+      <br />
+      {error && <p>{error}</p>}
+      <button className='button-form' type='submit'>
           <a id='submit-entrar' href='#'>Entrar</a>
           </button>
 
@@ -39,9 +106,10 @@ function Login() {
           <button>
           <Link to='/Cadastro' id='criar-conta'>Registrar</Link> 
           </button>
-        </div>
-    </main>
-  )
-}
+    </form>
+    </div>
+  );
+};
+
 
 export default Login;

f2998fcada331dade6b9653ab0ee34cd6da37b7e
Your NameTiagoCos <tiagocostasilva27@gmail.com>
2023-01-26 22:57:40 -0300

    Church

--- src/pages/Dashboard/Home.js
+++ src/pages/Dashboard/Home.js
@@ -2,12 +2,11 @@ import  '../Dashboard/indexHome.css';
 import { Alert } from 'antd';
 import Marquee from 'react-fast-marquee' ;
 import React from 'react';
-
+import { useState } from 'react';
 import { Avatar, Card } from 'antd';
 
 
 function Home(){
- 
 const msg = 'teste de mensagem'
 
 const { Meta } = Card;
@@ -16,11 +15,12 @@ return(
   <main className='principal'>
     <section id='a' className='primeiro'>
     <div className='Loop-Banner'>
-           <Alert  className='alert'
-              banner
-              message={
-              <Marquee pauseOnHover gradient={false}>{msg}</Marquee>}/>
-              </div>
+    <Alert  className='alert'
+      banner
+      message={
+     <Marquee pauseOnHover gradient={false}>{msg}</Marquee>}/>
+     </div>
+        <div className='Sub-div-img'>
          <span className='info-img'>
             <h1 className='Title'>Conheça nosso site </h1>
               <p className='Sub-Title'>Confira as informações mais relevantes pra você!</p>
@@ -29,27 +29,29 @@ return(
                  <a className='btn-a' href='#d'>Quem somos</a>
                 </button>
                </span>
-             <div className='Sub-div-img'></div>
+             </div>
           </section>  
-
       <section id='b' className='segundo'>  
       <div className='container-cards'>
-        <h1 className='titulo-cards'>Diretoria e cargos</h1>
-        </div>
-        <Card
+        <h1 className='titulo-cards'>Diretoria e cargos</h1></div>
+      <Card
         className='Card-identificação'
           cover={
-            <img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"/>}>
-             <b>Title</b>
-          <Meta avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-            description="This is the description" />
-        </Card>
+          <img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"/>}>
+          <b>Title</b>
+        <Meta avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}  description="This is the description" />
+      </Card>
       </section>
           
     <section id='c' className='terceiro'>
-            <video controls ><iframe width="560" height="315" src="https://www.youtube.com/embed/n7VZxg9pxkg?start=1"
-             title="YouTube video player" frameborder="0" allow="accelerometer; autoplay;
-             clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></video>
+    <div>
+      <video
+        
+        controls
+        style={{width: 700, color: 1}}
+      />
+      
+    </div>
         </section>
 
         <section id='d' className='quarto'>

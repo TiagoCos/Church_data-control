870eef3eab3a4d2675b2b08eb04eb8de4815de03
Your NameTiagoCos <tiagocostasilva27@gmail.com>
2023-04-14 22:35:30 -0300

    Church

--- src/Components/Home-components/filiacao.js
+++ src/Components/Home-components/filiacao.js
@@ -1,49 +1,70 @@
 import './filiacao.css'
+import React from 'react';
+import { useState } from 'react';
 
 
-function Filiacao(){
 
-    return(
-    <section id='d' className='quarto'>
-        <main className="conteudo">
-        <article className="cardF">
-            <h1>HTML</h1>
-            <span>
-                HTML é uma linguagem de marcação utilizada na construção de páginas na Web. Documentos HTML podem ser
-                interpretados por navegadores.
-            </span>
-        </article>
-        <article className="cardF">
-            <h1>CSS</h1>
-            <span>
-                Cascading Style Sheets é um mecanismo para adicionar estilo a um documento web.
-            </span>
-        </article>
-        <article className="cardF">
-            <h1>JS</h1>
-            <span>
-                JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem
-                dinâmica fraca e multiparadigma.
-            </span>
-        </article>
-        <article className="cardF">
-            <h1>GIT</h1>
-            <span>
-                Git ₍ₒᵤ em inglês britânico₎ é um sistema de controle de versões distribuído, usado principalmente no
-                desenvolvimento de software, mas pode ser usado para registrar o histórico de edições de qualquer tipo
-                de arquivo.
-            </span>
-        </article>
-        <article className="cardF">
-            <h1>React JS</h1>
-            <span>
-                O React é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas
-                web.
-            </span>
-        </article>
-    </main>
-     </section>
-    )
-}
+function Filiação({img1 = 'https://picsum.photos/200/300',
+ img2='https://picsum.photos/400/600',
+  img3='https://picsum.photos/500/500'}){
 
-export default Filiacao
+const handleRestart = () => {
+    setStep(0);
+  
+    };
+const [step, setStep] = useState(0);
+
+const handleNext = () => {
+    setStep((step) => step + 1);
+  };
+
+  const renderImg = (step) => {
+    switch (step) {
+      case 0:
+        return (
+          <div className="swiper-slide2">
+            <h1>01</h1>
+                <button 
+                    onClick={handleNext}
+                    className="filiacao_btn"
+                >Próximo</button>
+                <img className='fotos-back'  src={img1}></img>
+          </div>
+        );
+      case 1:
+        return (
+          <div className="swiper-slide2">
+            <h1>02</h1>
+            <button
+                    onClick={handleNext}
+                    className="filiacao_btn"
+                >Próximo</button>
+                <img className='fotos-back' type='image' src={img2}></img>
+          </div>
+        );
+      case 2:
+         return (
+             <div className="swiper-slide2">
+                <h1>03</h1>
+                <button
+                    onClick={handleRestart}
+                    className="filiacao_btn"
+                >Voltar</button>
+                <img className='fotos-back' type='image' src={img3}></img>
+            </div>
+            );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className="quarto" id='d'>
+      <div className="card2">
+        <div className="swiper2">{renderImg(step)}</div>
+      </div>
+    </div>
+  );
+  }
+
+export default Filiação;

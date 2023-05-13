5065500268559a67e457c5e33fb494bfd13cf0dd
Your NameTiagoCos <tiagocostasilva27@gmail.com>
2023-05-12 23:57:37 -0300

    Church

--- src/pages/Secretario/secretario.js
+++ src/pages/Secretario/secretario.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
 import MemberForm from "../../Components/MemberForm/MemberForm";
-import './index.secretario.css'
+import "./index.secretario.css";
+
 Modal.setAppElement("#root");
 
-const membersData = [
-  { id: 1, name: "João" },
-  { id: 2, name: "Maria" },
-  { id: 3, name: "José" },
-];
+const membersPerPage = 3;
 
 const Members = () => {
-  const [members, setMembers] = useState(membersData);
+  const [members, setMembers] = useState([
+    { id: 1, name: "João", address: "Rua A", phone: "1234-5678", email: "joao@teste.com" },
+    { id: 2, name: "Maria", address: "Rua B", phone: "9876-5432", email: "maria@teste.com" },
+    { id: 3, name: "José", address: "Rua C", phone: "5678-1234", email: "jose@teste.com" },
+  ]);
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const addMember = (member) => {
     setMembers([...members, member]);
@@ -31,49 +33,73 @@ const Members = () => {
     setModalIsOpen(false);
   };
 
+  const indexOfLastMember = currentPage * membersPerPage;
+  const indexOfFirstMember = indexOfLastMember - membersPerPage;
+  const currentMembers = members.slice(indexOfFirstMember, indexOfLastMember);
+
+  const totalPages = Math.ceil(members.length / membersPerPage);
+
+  const handleClick = (page) => {
+    setCurrentPage(page);
+  };
+
+  const renderPageNumbers = () => {
+    const pageNumbers = [];
+
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(i);
+    }
+
+    return pageNumbers.map((number) => (
+      <li key={number}>
+        <button className="page-link" onClick={() => handleClick(number)}>
+          {number}
+        </button>
+      </li>
+    ));
+  };
+
   return (
     <>
       <main className="main-ficha">
-      <h1 >Lista de Membros</h1>
-      <button className="Btn-ficha" onClick={openModal}>Adicionar Membro</button>
-      <table class="table-members">
-        <thead>
-          <tr >
-          <th class="table-header">Nome</th>
-          <th class="table-header">Civil</th>
-          <th class="table-header">Gênero</th>
-          <th class="table-header">Endereço</th>
-          <th class="table-header">Numero</th>
-          <th class="table-header">Email</th>
-          <th class="table-header">Ativo?</th>
-          <th class="table-header">Excluir</th>
-
-          </tr>
-        </thead>
-        <tbody>
-  {members.map((member) => (
-    <tr key={member.id}>
-      <td>{member.name}</td>
-      <td>{member.cpf}</td>
-      <td>{member.birthDate}</td>
-      <td>{member.maritalStatus}</td>
-      <td>{member.gender}</td>
-      <td>{member.address}</td>
-      <td>{member.phone}</td>
-      <td>{member.email}</td>
-      <td>{member.observation}</td>
-      <td>{member.isActive ? 'Sim' : 'Não'}</td>
-      <td>
-        <button  className="Btn-ficha" onClick={() => removeMember(member.id)}>Excluir</button>
-      </td>
-    </tr>
-  ))}
-</tbody>
-
-      </table>
-      <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
-        <MemberForm addMember={addMember} closeModal={closeModal} />
-      </Modal>
+        <h1>Lista de Membros</h1>
+        <button className="Btn-ficha" onClick={openModal}>
+          Adicionar Membro
+        </button>
+        <table className="table-members">
+          <thead>
+            <tr>
+              <th className="table-header">Nome</th>
+              <th className="table-header">Endereço</th>
+              <th className="table-header">Número</th>
+              <th className="table-header">Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentMembers.map((member) => (
+              <tr key={member.id}>
+                <td>{member.name}</td>
+                <td>{member.address}</td>
+                <td>{member.phone}</td>
+                <td>{member.email}</td>
+                <td>
+                  <button
+                    className="Btn-ficha"
+                    onClick={() => removeMember(member.id)}
+                  >
+                    Excluir
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="pagination">
+          <ul className="pagination-list">{renderPageNumbers()}</ul>
+        </div>
+        <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
+          <MemberForm addMember={addMember} closeModal={closeModal} />
+        </Modal>
       </main>
     </>
   );
